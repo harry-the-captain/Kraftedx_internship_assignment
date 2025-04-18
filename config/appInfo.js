77a02274e3926ae723b820f1b268ec0a8efd8b5f
harry-the-captain <2022bit064@sggs.ac.in>
2025-04-19 04:03:38 +0530

    Initial commit

--- config/appInfo.js
+++ config/appInfo.js
@@ -3,8 +3,12 @@ export const websiteDomain = process.env.NEXT_PUBLIC_APP_DOMAIN;
 
 export const appInfo = {
   appName: 'SuperTokens Demo App',
-  websiteDomain,
-  apiDomain: websiteDomain,
-  apiBasePath: '/api/auth',
+  // websiteDomain,
+  // apiDomain: websiteDomain,
+  // apiBasePath: '/api/auth',
+
+  apiDomain: process.env.NEXT_PUBLIC_API_DOMAIN || "http://localhost:3000",
+  websiteDomain: process.env.NEXT_PUBLIC_WEBSITE_DOMAIN || "http://localhost:3000",
+  apiBasePath: "/api/auth",
   websiteBasePath: '/auth',
 };
